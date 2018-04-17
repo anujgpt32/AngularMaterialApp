0b878afcea9f7c8d0734c9ed9ef0f998f77a9ad3
Anuj <anujgpt32@gmail.com>
2018-04-17 13:34:16 +0530

    Login and Sign up forms UI done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms'
-import { MatInputModule, MatCardModule, MatCheckboxModule } from '@angular/material'
+import { MatInputModule, MatCardModule, MatCheckboxModule, MatTabsModule, MatButtonModule } from '@angular/material'
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
  
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
+let authRoutes:Routes = [
+
+];
+let homeChildRoutes:Routes = [
+
+];
+let routes:Routes = [
+    {
+        path:'',
+        component:AuthComponent,
+        children:authRoutes
+    },
+    {
+        path:'auth',
+        component:AuthComponent,
+        children:authRoutes
+    },
+    {
+        path:'home',
+        component:HomeComponent,
+        children:homeChildRoutes
+    }
+];
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AuthComponent,
+        HomeComponent,
+        LoginComponent,
+        SignupComponent
     ],
     imports: [
         BrowserModule,
@@ -17,7 +50,10 @@ import { AppComponent } from './app.component';
         FormsModule,
         MatCheckboxModule,
         MatCardModule,
-        MatInputModule
+        MatInputModule,
+        MatTabsModule,
+        MatButtonModule,
+        RouterModule.forRoot(routes)
     ],
     providers: [],
     bootstrap: [AppComponent]
